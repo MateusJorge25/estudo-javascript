6928e4110a18856701710c499d79d15a5f1110c6
Mateusj2506 <mateus_jorge2002@outlook.com>
2024-05-17 11:21:41 -0300

    variaveis

--- puro.js
+++ puro.js
@@ -4,9 +4,13 @@ let nameClient = "Mateus"
 
 let ageClient = 21
 
+let cityClient = "Jaboticabal"
 
+let addressClient = "Jardim Grajau"
 
+let workClient = "Desenvolvedor junior"
 
-document.write("Hello, World!!")
+document.write("Ola "+nameClient+" sua idade e "+ageClient+"anos"+" Voce e natural da cidade de "+cityClient+" mora no bairro "+addressClient+" e trabalha como "+workClient)  //document.write serve para mostrar na tela como o print
+
 
 
