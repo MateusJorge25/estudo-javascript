715b4c19cea9ec65c67a48405f12611e23004e62
Mateusj2506 <mateus_jorge2002@outlook.com>
2024-05-20 18:11:11 -0300

    novos arquivos

---
+++ estDecisao.js
@@ -0,0 +1,13 @@
+
+let salario = prompt("Digite seu salario:")
+
+if(salario >= 2000) {
+    document.write("foi aprovado "+salario+"<br />")
+}
+
+
+else{
+    document.write("foi reprovado "+salario+"<br />")
+}
+
+
---
+++ switch.js
@@ -0,0 +1,38 @@
+
+let pesquisa = prompt ("Qual celular voce prefere: 1-Sansung 2-Iphone 3-Motorola ")
+pesquisa = Number(pesquisa)     // serve para saber o tipo de dado que a vareavel interpretara 
+
+switch(pesquisa){
+
+    case 1:
+    document.write("Sansung: celular com otima bateria")
+    breack
+
+    case 2:
+    document.write("Iphone: otimas cameras")
+    breack
+
+    case 3:
+    document.write("Motorola: Custo beneficio")
+    breack
+}
+
+
+/*let salario = prompt("Qual seu salario R$: 1000 2000 3000")
+salario = Number(salario)
+
+
+switch(salario){
+    
+    case 1000:
+    document.write("R$ 1000: Voce esta ganhando um salario minimo")
+    breack
+
+    case 2000:
+    document.write("R$ 2000: Esta ganhando um salario OK")
+    breack
+
+    case 3000:
+    document.write("R$ 3000: Ja esta ganhando um otimo salario")
+    breack
+}*/
